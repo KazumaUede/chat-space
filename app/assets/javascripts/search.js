13ba386ffddb6f54faaf8757ab2b39252796b959
KazumaUede <katsuodashi794@gmail.com>
2018-09-18 17:40:37 +0900

    Get users name Ajax

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,5 +2,27 @@ $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     console.log(input);
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+   .done(function(users) {
+    console.log("成功");
+    console.log(users);
+     $("#user-search-result").empty();
+     if (users.length !== 0) {
+       // users.forEach(function(user){
+       //   appendProduct(user);
+       // });
+     }
+     else {
+       // appendNoUser("一致する映画はありません");
+     }
+   })
+
+
   });
 });
