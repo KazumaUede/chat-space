79194d1ee51de64f55e907821005583ace84d1d7
KazumaUede <katsuodashi794@gmail.com>
2018-09-19 11:31:44 +0900

    Add incremental search

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,28 +1,72 @@
-$(function() {
-  $("#user-search-field").on("keyup", function() {
+$(function () {
+  var search_list = $("#user-search-result");
+  var member_list = $("#chat-group-users");
+
+  function appendUser(user) {
+    var html = `
+                <div class="chat-group-user result clearfix">
+                  <p class="chat-group-user__name">${ user.name }</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }"data-user-name="${ user.name }">追加</a>
+                </div>`
+    search_list.append(html);
+  }
+
+  function appendNoUser(user) {
+    var html = `
+                <div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user}</p>
+                </div>
+              `
+    search_list.append(html);
+  }
+
+  function appendAddUser(userid,username) {
+    var html = `
+                <div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ userid }'>
+                  <p class='chat-group-user__name'>${ username }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    member_list.append(html);
+  }
+
+
+  $("#user-search-field").on("keyup", function () {
     var input = $("#user-search-field").val();
-    console.log(input);
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-
-   .done(function(users) {
+    var preInput = "";
+    if (input !== preInput) {
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: {
+          keyword: input
+        },
+        dataType: 'json'
+      }).done(function (users) {
+        // console.log("成功");
+        console.log(users);
+        $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function (user) {
+            appendUser(user);
+          });
+        } else {
+          appendNoUser("一致するユーザーはありません");
+        }
+      })
+    } else {
+      $(".result").remove();
+    }
+  });
+  $(document).on('click','.chat-group-user__btn--add',function(){
     console.log("成功");
-    console.log(users);
-     $("#user-search-result").empty();
-     if (users.length !== 0) {
-       // users.forEach(function(user){
-       //   appendProduct(user);
-       // });
-     }
-     else {
-       // appendNoUser("一致する映画はありません");
-     }
-   })
-
-
+    var userId = $(this).attr('data-user-id')
+    var userName = $(this).attr('data-user-name')
+    appendAddUser(userId, userName)
+    $(this).parent().remove();
+  });
+  $(document).on('click','.chat-group-user__btn--remove',function(){
+    console.log("成功");
+    $(this).parent().remove();
   });
 });
