42a6f7c9952742577b144011ddfe8d4fd7f26ba9
KazumaUede <katsuodashi794@gmail.com>
2018-09-24 12:05:14 +0900

    Fix up

--- Gemfile
+++ Gemfile
@@ -42,11 +42,7 @@ group :development, :test do
   gem 'rails-controller-testing'
   gem 'factory_girl_rails', '~> 4.0'
   gem 'faker'
-  gem 'capistrano'
-  gem 'capistrano-rbenv'
-  gem 'capistrano-bundler'
-  gem 'capistrano-rails'
-  gem 'capistrano3-unicorn'
+
 end
 
 group :development do
@@ -72,3 +68,12 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :production do
   gem 'unicorn'
 end
+
+group :development, :test do
+  gem 'capistrano'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano3-unicorn'
+end
+
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,11 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+# lock '3.11.0'
 
-set :application, 'Chat-space'
-set :repo_url,  'git@github.com:KazumaUede/Chat-space.git'
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, 'chat-space'
+set :repo_url,  'git@github.com:KazumaUede/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -22,7 +25,6 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
-
 
 
 # config valid for current version and patch releases of Capistrano
