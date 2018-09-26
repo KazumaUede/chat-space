4c3616671f71506ed6232c296ea1735dfc41ded3
KazumaUede <katsuodashi794@gmail.com>
2018-09-26 11:35:23 +0900

    Fix js and test

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/uploads/message/image/2/flower.jpeg")
+    image File.open("#{Rails.root}/public/uploads/message/image/398/flower.jpeg")
     user
     group
   end
