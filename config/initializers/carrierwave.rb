1aa5ee17edba29481d41b13359d30eae4d84d6b7
KazumaUede <katsuodashi794@gmail.com>
2018-09-27 12:01:44 +0900

    Fix S3 address

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,9 +9,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
   config.fog_directory  = 'katsuodashi-upload'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/katsuodashi-upload'
 end
